7fbcf0ad21dc3857c6f3e8651f4cd7ddbde9793b
VanNguyen21 <laivannguyen.97@gmail.com>
2019-02-20 15:11:39 +0700

    Fix Chapter 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
@@ -8,13 +7,13 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = t "invalid "
+      flash.now[:danger] = t "invalid"
       render :new
+    end
   end
 
   def destroy
     log_out
     redirect_to root_path
   end
-
 end
